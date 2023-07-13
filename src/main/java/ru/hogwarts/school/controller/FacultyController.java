f03d9e9058b706c3ad0acbe911d2e37821d88a22
Алексей <balko-aleksey@yandex.ru>
2023-07-13 22:03:56 +0700

    Home Work partially solved

---
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -0,0 +1,45 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.service.FacultyService;
+import java.util.Collection;
+
+@RestController
+@RequestMapping("faculty")
+public class FacultyController {
+        private final FacultyService facultyService;
+
+        public FacultyController(FacultyService facultyService) {
+            this.facultyService = facultyService;
+        }
+        @GetMapping("{id}")
+        public ResponseEntity<Faculty> getFacultyInfo(@PathVariable Long id){
+            Faculty faculty = facultyService.findFaculty(id);
+            if (faculty == null){
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(faculty);
+        }
+        @GetMapping
+        public ResponseEntity<Collection<Faculty>> getAllFaculty(){
+            return ResponseEntity.ok(facultyService.getAllFaculties());
+        }
+        @PostMapping
+        public Faculty createFaculty(@RequestBody Faculty faculty){
+            return facultyService.createFaculty(faculty);
+        }
+        @PutMapping
+        public ResponseEntity<Faculty> editFaculty(@RequestBody Faculty faculty){
+            Faculty foundFaculty = facultyService.editFaculty(faculty);
+            if (foundFaculty == null){
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(foundFaculty);
+        }
+        @DeleteMapping("{id}")
+        public Faculty deleteFaculty(@PathVariable Long id){
+            return facultyService.deleteFaculty(id);
+        }
+}
